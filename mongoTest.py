47c36e9f79356ad55e5473c54475ee32467e148c
Fabian Kemper <fabian.kemper@mailbox.org>
2022-11-06 15:32:02 +0000

    mongoTest.py ist nun bereit die DB zu benchmarken

--- mongoTest.py
+++ mongoTest.py
@@ -2,12 +2,12 @@ from pymongo import MongoClient
 from datetime import datetime
 
 
-iterations = 3
+iterations = 10000
 
 
 datetimeObj = datetime.now()
 collectionName = "collection_" +  datetimeObj.strftime("%d_%m__%H_%M")
-
+# collectionName = "collection_06_11__14_54"
 print(collectionName)
 
 client = MongoClient('mongodb://localhost:27017/')
@@ -26,10 +26,18 @@ with client:
     print("im here")
 
     for i in range(0,iterations):
-        lastResult = collection.find_one()
+        lastResult = collection.find_one(sort=[( "index", -1 )])
+        #print(lastResult)
         index = lastResult["index"] + 1
         timestamp = datetime.now().timestamp()
         oldTimestamp = lastResult["timestamp"]
         timeSinceLastInsert = timestamp - oldTimestamp
         input = {"index": index,"timestamp":timestamp,"timeSinceLastInsert":timeSinceLastInsert}
         collection.insert_one(input)
+        
+
+finishTime = datetime.now()
+
+runningTime = finishTime - datetimeObj
+avgTime = (runningTime/iterations).total_seconds() * 1000
+print(f"Runningtime: {runningTime} for {iterations} iterations. Average Time: {avgTime} ms")
