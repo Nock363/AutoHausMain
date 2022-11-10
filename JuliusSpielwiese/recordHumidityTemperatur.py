cbba779b57d8b01d61a624496303ddbeb7c30321
Fabian Kemper <fabian.kemper@mailbox.org>
2022-11-10 22:49:20 +0000

    updating commit

--- JuliusSpielwiese/recordHumidityTemperatur.py
+++ JuliusSpielwiese/recordHumidityTemperatur.py
@@ -1,38 +1,30 @@
+import sys
+sys.path.insert(0, '../python_sensor_aht20/')
 import AHT20
 from datetime import datetime
 
 
 #Inititalisiere AHT20
 aht20 = AHT20.AHT20()
-dt = datetime.now()
 
 # Convert to two decimal places cleanly
 # round() won't include trailing zeroes
 def round_num(input):
    return '{:.2f}'.format(input)
 
+iterations = 100
 
 
-
-with open("tempLog.csv", "a") as f:
-	f.write(str(dt))
-	f.write(", ")
-	f.write(str(round_num(aht20.get_temperature())))
-	f.write("\n")
-
-with open("humidLog.csv", "a")as f:
-	f.write(str(dt))
-	f.write(", ")
-	f.write(str(round_num(aht20.get_humidity())))
-	f.write("\n")
-
-with open("tempHudLog.csv", "a") as f:
-	f.write(str(dt))
-	f.write(", ")
-	f.write(str(round_num(aht20.get_temperature())))
-	f.write(", ")
-	f.write(str(round_num(aht20.get_humidity())))
-	f.write("\n")
+with open("tempHudLog_stressTest.csv", "a") as f:
+	for i in range(0,iterations):
+		print(f"iteration {i}")
+		dt = datetime.now()
+		f.write(str(dt))
+		f.write(", ")
+		f.write(str(round_num(aht20.get_temperature())))
+		f.write(", ")
+		f.write(str(round_num(aht20.get_humidity())))
+		f.write("\n")
 
 
 
