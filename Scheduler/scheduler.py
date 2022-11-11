37f8a161bf2e3fe9f48a0f9a98ecfa48ca525bd8
Fabian Kemper <fabian.kemper@mailbox.org>
2022-11-11 00:27:39 +0000

    Scheduler angefangen zu bauen.

---
+++ Scheduler/scheduler.py
@@ -0,0 +1,35 @@
+import multiprocessing
+import time
+import logging
+
+from schedulerTestProgramm import Test
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
+def threadRunner(obj):
+    obj.run()
+
+tests = []
+processes = []
+processesCount = 10;
+for i in range(0,processesCount):
+        test = Test(1,f"Ich bin test ({i})")
+        process = multiprocessing.Process(target=threadRunner,args=(test,))
+        tests.append(test)
+        processes.append(process)
+
+
+# test1 = Test(1,"Ich bin test1")
+# test2 = Test(1,"Ich bin test2")
+
+# thread1 = multiprocessing.Process(target=threadRunner,args=(test1,))
+# thread2 = multiprocessing.Process(target=threadRunner,args=(test2,))
+
+# processes.append(thread1)
+# processes.append(thread2)
+for p in processes:
+        p.start()
+
+time.sleep(5)
+for p in processes:
+        p.terminate()
+
