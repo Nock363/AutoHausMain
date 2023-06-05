8cf854590e4503213f6dfebd6482ab230f0f37f7
Fabian Kemper <fabian.kemper@maailbox.org>
2023-06-05 22:53:11 +0100

    Dünger Pumpe ist jetzt mit parametern startbar

--- i2cArduino_Test.py
+++ i2cArduino_Test.py
@@ -4,6 +4,20 @@ import smbus
 import struct
 import time
 
+if len(sys.argv) != 3:
+    print("Please provide two integer numbers as start parameters.")
+    print("The First 1-3 refers to the Pump Number, the second is the Time in seconds")
+    print("for example: python3 i2cArduino_Test.py 2 4")
+    sys.exit(1)
+
+
+try:
+    pump = int(sys.argv[1])
+    duration = int(sys.argv[2])
+except ValueError:
+    print("Invalid start parameters. Please provide two integer numbers.")
+    sys.exit(1)
+
 # Define I2C bus number and Arduino slave address
 bus = smbus.SMBus(3)
 address = 0x8
@@ -31,16 +45,16 @@ DATA_FORMAT = 'ii'  # Format string for a float and an integer
 # while True:
 #dataA is defined as hex value
 
-duration = 1
+#duration = 1
 
-data1 = struct.pack(DATA_FORMAT, 1, duration)
-data2 = struct.pack(DATA_FORMAT, 2, duration)
-data3 = struct.pack(DATA_FORMAT, 3, duration)
+data1 = struct.pack(DATA_FORMAT, pump, duration)
+#data2 = struct.pack(DATA_FORMAT, 2, duration)
+#data3 = struct.pack(DATA_FORMAT, 3, duration)
 bus.write_i2c_block_data(address, 0, list(data1))
-time.sleep(1.3)
-bus.write_i2c_block_data(address, 0, list(data2))
-time.sleep(1.3)
-bus.write_i2c_block_data(address, 0, list(data3))
+#time.sleep(1.3)
+#bus.write_i2c_block_data(address, 0, list(data2))
+#time.sleep(1.3)
+#bus.write_i2c_block_data(address, 0, list(data3))
 
 #get_data()
 
